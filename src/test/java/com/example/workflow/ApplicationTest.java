496d6fbfbf226688b332f98883f04d594762f638
Radoslaw Postolowicz <radoslaw.postolowicz@cronn.de>
2022-02-08 18:48:08 +0100

    Add bug test case

--- src/test/java/com/example/workflow/ApplicationTest.java
+++ src/test/java/com/example/workflow/ApplicationTest.java
@@ -1,12 +1,28 @@
 package com.example.workflow;
 
+import org.camunda.bpm.client.ExternalTaskClient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ApplicationTest {
 
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
     @Test
-    void test() {
+    void shouldStopExternalTaskClientWithoutWaitingFor1Minute() throws InterruptedException {
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest/")
+                .asyncResponseTimeout(60_000)
+                .disableBackoffStrategy()
+                .build();
+
+        client.subscribe("sampleTopic")
+                .handler((externalTask, externalTaskService) -> externalTaskService.complete(externalTask))
+                .open();
+
+        client.stop();
     }
 }
