65dce9b28a15f9363de97705da00ef680d7cfa2f
Radoslaw Postolowicz <radoslaw.postolowicz@cronn.de>
2022-02-08 19:11:50 +0100

    Add another bug case

--- src/test/java/com/example/workflow/ApplicationTest.java
+++ src/test/java/com/example/workflow/ApplicationTest.java
@@ -1,10 +1,13 @@
 package com.example.workflow;
 
+import com.sun.management.OperatingSystemMXBean;
+import org.assertj.core.api.Assertions;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.management.ManagementFactory;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -24,5 +27,19 @@ class ApplicationTest {
                 .open();
 
         client.stop();
+    }
+
+    @Test
+    void shouldNotOverheatCpuWhenNoTopicsToSubscribe() throws InterruptedException {
+        ExternalTaskClient client = ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest/")
+                .build();
+
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        long startCpuUsage = osBean.getProcessCpuTime();
+        Thread.sleep(5_000);
+        long endCpuUsage = osBean.getProcessCpuTime();
+        long cpuUsage = endCpuUsage - startCpuUsage;
+        Assertions.assertThat(cpuUsage).isLessThan(1_000_000_000); // 1s = 1_000_000_000ns
     }
 }
